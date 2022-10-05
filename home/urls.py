5dca7a6694b450f80dea310c6232c4b1110bce5a
simonvaru <108592494+simonvaru@users.noreply.github.com>
2022-10-05 19:23:57 -0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-# from .views import hola, fecha
 from home import views
-# from . import views
+
 
 urlpatterns = [
     path('', views.index),
-    path('hola/', views.hola),
-    path('fecha/', views.fecha),
-    path('fecha-nacimiento/<int:edad>', views.calcular_fecha_nacimiento),
     path('mi-template/', views.mi_template),
     path('mi-template/<str:nombre>', views.tu_template),
     path('prueba-template/', views.prueba_template),
